083b806536196fe3d5c10304ae05609b2b9548bc
Nathan Verzemnieks <njvrzm@gmail.com>
2018-12-09 14:44:01 -0800

    Day 9 part 1: it works! but not fast enough

---
+++ day_9/d9p1.py
@@ -0,0 +1,23 @@
+from itertools import cycle
+import sys
+
+nelfs, narbles = map(int, sys.argv[1:])
+elves = list(range(nelfs))
+
+board = [0]
+score = {elf:0 for elf in elves}
+
+current = 0 # place, not marble
+
+for elf, marble in zip(cycle(elves), range(1, narbles)):
+    if marble % 23 == 0:
+        bonus = (current - 7) % len(board)
+        score[elf] += marble + board[bonus]
+        board = board[:bonus] + board[bonus + 1:]
+        current = bonus
+    else:
+        current = (current + 2) % len(board)
+        board = board[:current] + [marble] + board[current:]
+
+print(max(score.values()))
+
